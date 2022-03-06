a2632c39566c2260cd5c1bf5b02c445da75d62c4
Ubuntu <ubuntu@ip-172-31-29-252.eu-central-1.compute.internal>
2022-03-06 22:06:47 +0000

    new results constant time with constant throughput 20 tasks per second

--- benchmarks/run_visualization.py
+++ benchmarks/run_visualization.py
@@ -4,8 +4,8 @@ from main import run_test
 if __name__ == '__main__':
     ENVIRONMENT = os.getenv("ENVIRONMENT")
 
-    user_count = 10
-    spawn_rate = 10
+    user_count = 20
+    spawn_rate = 20
     runtime = "31s"
 
     run_test(ENVIRONMENT, "visualization", ["rasdaman-local", "rastless", "rasdaman-proxy"], user_count, spawn_rate,
