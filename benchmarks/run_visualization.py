5e37e21d860c275ac14ff4169047488537d9c62b
Ubuntu <ubuntu@ip-172-31-29-252.eu-central-1.compute.internal>
2022-03-07 19:43:12 +0000

    new results

--- benchmarks/run_visualization.py
+++ benchmarks/run_visualization.py
@@ -4,9 +4,9 @@ from main import run_test
 if __name__ == '__main__':
     ENVIRONMENT = os.getenv("ENVIRONMENT")
 
-    user_count = 20
-    spawn_rate = 20
-    runtime = "61s"
+    user_count = 25
+    spawn_rate = 1
+    runtime = "41s"
 
-    run_test(ENVIRONMENT, "visualization", ["rasdaman-local", "rasdaman-proxy"], user_count, spawn_rate,
+    run_test(ENVIRONMENT, "visualization", ["rasdaman-local", "rastless", "rasdaman-proxy"], user_count, spawn_rate,
              runtime, add_timestamp=True)
