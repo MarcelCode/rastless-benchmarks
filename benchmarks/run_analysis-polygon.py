ceed770c48d339869e3045a2735288aef47dee1c
Marcel <siegmann@eomap.de>
2022-03-09 01:15:34 +0100

    new results

--- benchmarks/run_analysis-polygon.py
+++ benchmarks/run_analysis-polygon.py
@@ -4,8 +4,8 @@ from main import run_test
 if __name__ == '__main__':
     ENVIRONMENT = os.getenv("ENVIRONMENT")
 
-    user_count = 25
-    spawn_rate = 1
+    user_count = 10
+    spawn_rate = 10
     runtime = "41s"
 
     run_test(ENVIRONMENT, "polygon-analysis", ["rasdaman-local", "rastless", "rasdaman-proxy"], user_count, spawn_rate,
